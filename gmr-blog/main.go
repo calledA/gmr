6fc5df79f746aac68ab8996edff0c614433d3611
calledA <iamwzxin@163.com>
2022-06-23 14:57:30 +0800

    master

--- gmr-blog/main.go
+++ gmr-blog/main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"gmr/gmr-blog/pkg/setting"
 	"gmr/gmr-blog/routers"
+	"net/http"
+
 )
 
 func main() {
 	router := routers.InitRouter()
 	s := &http.Server{
-		Addr:fmt.Sprintf("%d",setting.HTTPPort),
-		Handler:router,
-		ReadTimeout:setting.ReadTimeout,
-		WriteTimeout:setting.WriteTimeout,
-		MaxHeaderBytes:1 << 20,
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
-
+	fmt.Println("服务器开始启动")
 	s.ListenAndServe()
+	fmt.Println("服务器关闭")
+
 }
--- gmr-blog/routers/router.go
+++ gmr-blog/routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"gmr/gmr-blog/pkg/setting"
+	"gmr/gmr-blog/routers/api"
+	"gmr/gmr-blog/routers/api/v1"
+	"gmr/gmr-blog/middleware/jwt"
 
 	"github.com/gin-gonic/gin"
-	"gmr/gmr-blog/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -13,12 +15,35 @@ func InitRouter() *gin.Engine {
 	router.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
 
-	appv1 := router.Group("/api/v1")
+	router.GET("/auth",api.GetAuth)
+
+	apiv1 := router.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
-		appv1.GET("/tags",v1.GetTags)
-		appv1.POST("/tags",v1.AddTag)
-		appv1.PUT("/tags/:id",v1.EditTag)
-		appv1.DELETE("/tags/:id",v1.DeleteTag)
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//添加标签列表
+		apiv1.POST("/tags", v1.AddTag)
+		//编辑标签列表
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除标签列表
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
 	}
+
+	apiv2 := router.Group("/api/v2")
+	apiv2.Use(jwt.JWT())
+	{
+		//获取文章列表
+		apiv2.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv2.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv2.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv2.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv2.DELETE("/articles/:id", v1.DeleteArticle)
+	}
+
 	return router
 }
